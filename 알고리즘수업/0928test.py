9ee60e633dd1edb54d71df1b43330214861b455d
Aden-Jang <wrd4000@gmail.com>
2022-09-30 08:12:50 +0900

    220930 0812 update

--- 알고리즘수업/0928test.py
+++ 알고리즘수업/0928test.py
@@ -60,36 +60,152 @@
 #     # print(f'#{tc} {sum(vis)-1}')
 #     print(*adjM, sep='\n')
 #     print(vis)
+#
+# # 상원이의 생일파티 (5521_bfs)
+# def bfs(N):
+#     q = []              # 큐 생성
+#     vis = [0]*(N+1)     # vis 생성
+#     q.append(1)         # 시작점 인큐
+#     vis[1] = 1          # 시작점 방문 표시
+#     while q:            # 큐가 비어있지 않으면
+#         t = q.pop(0)
+#         if vis[t] > 3:
+#             break
+#         for i in range(1, N+1):
+#             if adj[t][i] == 1 and vis[i] == 0:
+#                 q.append(i)
+#                 vis[i] = vis[t] + 1
+#     cnt = 0
+#     for i in range(1, N+1):
+#         if 1 < vis[i] < 4:
+#             cnt += 1
+#     return cnt
+#
+#
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, M = map(int, input().split())
+#     adj = [[0]*(N+1) for _ in range(N+1)]
+#     for _ in range(M):
+#         a, b = map(int, input().split())
+#         adj[a][b] = 1
+#         adj[b][a] = 1
+#     ans = bfs(N)
+#
+#     print(f'#{tc} {ans}')
 
-# 상원이의 생일파티 (5521_bfs)
-def bfs(N):
-    q = []              # 큐 생성
-    vis = [0]*(N+1)     # vis 생성
-    q.append(1)         # 시작점 인큐
-    vis[1] = 1          # 시작점 방문 표시
-    while q:            # 큐가 비어있지 않으면
-        t = q.pop(0)
-        if vis[t] > 3:
+# # mst1
+# '''
+# 6 11
+# 0 1 32
+# 0 2 31
+# 0 5 60
+# 0 6 51
+# 1 2 21
+# 2 4 46
+# 2 6 25
+# 3 4 34
+# 3 5 18
+# 4 5 40
+# 4 6 51
+# '''
+#
+# V, E = map(int, input().split())
+# adjM = [[0]*(V+1) for _ in range(V+1)]
+# adjL = [[] for _ in range(V+1)]
+# for _ in range(E):
+#     u, v, w = map(int, input().split())
+#     adjM[u][v] = w
+#     adjM[v][u] = w      # 가중치가 있는 무방향 그래프
+#     adjL[u].append((v, w))
+#     adjL[v].append((u, w))
+#
+# # prim1
+#
+# def prim1(r, V):
+#     MST = [0]*(V+1)         # MST 포함여부
+#     key = [10000]*(V+1)     # 가중치의 최대값 이상으로 초기화, key[v]는 v가 MST에 속한 정점과 연결될 때 필요한 비용
+#     key[r] = 0              # 시작 정점의 key
+#     for _ in range(V):      # V+1개의 정점 중 V개를 선택
+#         # MST에 포함되지 않은 정점 중(MST[u]==0), key가 최소인 u찾기
+#         u = 0
+#         minV = 10000
+#         for i in range(V+1):
+#             if MST[i] == 0 and key[i] < minV:
+#                 u = i
+#                 minV = key[i]
+#         MST[u] = 1          # 정점 u를 MST에 추가
+#         # u에 인접인 v에 대해, MST에 포함되지 않은 정점이면
+#         for v in range(V+1):
+#             if MST[v] == 0 and adjM[u][v] > 0:
+#                 key[v] = min(key[v], adjM[u][v])    # u를 통해 MST에 포함되는 비용과 기존 비용 비교
+#     return sum(key)
+#
+#
+# V, E = map(int, input().split())
+# adjM = [[0]*(V+1) for _ in range(V+1)]
+# adjL = [[] for _ in range(V+1)]
+# for _ in range(E):
+#     u, v, w = map(int, input().split())
+#     adjM[u][v] = w
+#     adjM[v][u] = w      # 가중치가 있는 무방향 그래프
+#
+# print(prim1(0, V))
+#
+# # prim2
+#
+# def prim2(r, V):
+#     MST = [0]*(V+1)         # MST 포함여부
+#     MST[r] = 1              # 시작정점 표시
+#     s = 0                   # MST 간선의 가중치 합
+#     for _ in range(V):
+#         u = 0
+#         minV = 10000
+#         for i in range(V+1):        # MST에 포함된 정점i와 인접한 정점j 중 MST에 포함된 정점이면
+#             if MST[i] == 1:
+#                 for j in range(V+1):
+#                     if adjM[i][j] > 0 and MST[j] == 0 and minV > adjM[i][j]:
+#                         u = j
+#                         minV = adjM[i][j]
+#         s += minV
+#         MST[u] = 1
+#     return s
+#
+# V, E = map(int, input().split())
+# adjM = [[0]*(V+1) for _ in range(V+1)]
+# for _ in range(E):
+#     u, v, w = map(int, input().split())
+#     adjM[u][v] = w
+#     adjM[v][u] = w      # 가중치가 있는 무방향 그래프
+#
+# print(prim2(0, V))
+
+# kruskal1
+
+def fs(x):
+    while x != rep[x]:
+        x = rep[x]
+    return x
+
+def union(x, y):
+    rep[fs(y)] = fs(x)
+
+V, E = map(int, input().split())        # V 마지막 정점, 0~V번 정점. 개수 (V+1)개
+edge = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edge.append([u, v, w])
+edge.sort(key=lambda x:x[2])
+rep = [i for i in range(V+1)]           # 대표원소 배열
+
+N = V + 1       # 실제 정점 수
+cnt = 0         # 선택한 edge의 수
+total = 0       # MST 가중치의 합
+for u, v, w in edge:
+    if fs(u) != fs(v):
+        cnt += 1
+        union(u, v)
+        total += w
+        if cnt == N-1:  # 간선 수
             break
-        for i in range(1, N+1):
-            if adj[t][i] == 1 and vis[i] == 0:
-                q.append(i)
-                vis[i] = vis[t] + 1
-    cnt = 0
-    for i in range(1, N+1):
-        if 1 < vis[i] < 4:
-            cnt += 1
-    return cnt
-
-
-T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int, input().split())
-    adj = [[0]*(N+1) for _ in range(N+1)]
-    for _ in range(M):
-        a, b = map(int, input().split())
-        adj[a][b] = 1
-        adj[b][a] = 1
-    ans = bfs(N)
-
-    print(f'#{tc} {ans}')
+print(total)
--- 알고리즘수업/algorithm.md
+++ 알고리즘수업/algorithm.md
@@ -1935,3 +1935,63 @@ step 2. 후위 표기법의 수식을 스택을 이용하여 계산
             - Find-Set을 행하는 과정에서 만나는 모든 노드들이 직접 root를 가리키도록 포인터를 바꾸어 준다.
             - Path Compression의 예
                 ![Path Compression의 예](algorithm.assets/path%20compression%EC%98%88.JPG)
+
+### 최소 비용 신장 트리(MST)
+- 그래프에서 최소 비용 문제
+    1. 모든 정점을 연결하는 간선들의 가중치의 합이 최소가 되는 트리
+    2. 두 정점 사이의 최소 비용의 경로 찾기
+- 신장 트리
+    - n개의 정점으로 이루어진 무방향 그래프에서 n개의 정점과 n-1개의 간선으로 이루어진 트리
+- 최소 신장 트리(Minimum Spanning Tree)
+    - 무방향 가중치 그래프에서 신장 트리를 구성하는 간선들의 가중치의 합이 최소인 신장 트리
+- MST 표현
+    ![MST 표현1](algorithm.assets/MST%ED%91%9C%ED%98%841.JPG)
+    ![MST 표현2](algorithm.assets/MST%ED%91%9C%ED%98%842.JPG)
+- Prim 알고리즘
+    - 하나의 정점에서 연결된 간선들 중에 하나씩 선택하면서 MST를 만들어 가는 방식
+        1. 임의 정점을 하나 선택해서 시작
+        2. 선택한 정점과 인접하는 정점들 중의 최소 비용의 간선이 존재하는 정점을 선택
+        3. 모든 정점이 선택될 때 까지 1, 2 과정을 반복
+    - 서로소인 2개의 집합(2 disjoint-sets) 정보를 유지
+        - 트리 정점들(tree vertices) - MST를 만들기 위해 선택된 정점들
+        - 비트리 정점들(nontree vertices) - 선택 되지 않은 정점들
+    - 알고리즘 : Prim
+        ![prim 알고리즘](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98.JPG)
+    - Prim 알고리즘 적용 예
+    ![prim 알고리즘 예1](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%881.JPG)
+    ![prim 알고리즘 예2](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%882.JPG)
+    ![prim 알고리즘 예3](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%883.JPG)
+    ![prim 알고리즘 예4](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%884.JPG)
+    ![prim 알고리즘 예5](algorithm.assets/prim%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%885.JPG)
+- KRUSKAL 알고리즘
+    - 간선을 하나씩 선택해서 MST를 찾는 알고리즘
+        1. 최초, 모든 간선을 가중치에 따라 **오름차순**으로 정렬
+        2. 가중치가 가장 낮은 간선부터 선택하면서 트리를 증가시킴
+            - 사이클이 존재하면 다음으로 가중치가 낮은 간선 선택
+        3. n-1개의 간선이 선택될 때까지 2를 반복
+        ![KRUSKAL 알고리즘 표현](algorithm.assets/Kruskal%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%ED%91%9C%ED%98%84.JPG)
+    - 알고리즘 : KRUSKAL
+        ![KRUSKAL 알고리즘](algorithm.assets/kruskal%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98.JPG)
+    - KRUSKAL 알고리즘 적용 예
+    ![kruskal 알고리즘 예1](algorithm.assets/kruskal%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%881.JPG)
+    ![kruskal 알고리즘 예2](algorithm.assets/kruskal%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%882.JPG)
+    ![kruskal 알고리즘 예3](algorithm.assets/kruskal%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%EC%98%883.JPG)
+
+### 최단 경로
+- 최단 경로 정의
+    - 간선의 가중치가 있는 그래프에서 두 정점 사이의 경로들 중에 간선의 가중치의 합이 최소인 경로
+- 하나의 시작 정점에서 끝 정점까지의 최단경로
+    - 다익스트라(dijkstra) 알고리즘
+        - 음의 가중치를 허용하지 않음
+    - 벨만-포드(Bellman-Ford) 알고리즘
+        - 음의 가중치 허용
+- 모든 정점들에 대한 최단 경로
+    - 플로이드-워샬(Floyd-Warshall) 알고리즘
+- 시작 정점에서 거리가 최소인 정점을 선택해 나가면서 최단 경로를 구하는 방식
+- 시작 정점(s)에서 끝 정점(t) 까지의 최단 경로에 정점 x가 존재한다.
+- 최단 경로는 s에서 x까지의 최단 경로와 x에서 t까지의 최단 경로로 구성된다.
+- 탐욕 기법을 사용한 알고리즘으로 MST의 프림 알고리즘과 유사하다.
+- Dijkstra 알고리즘 표현
+    ![Dijkstra 알고리즘 표현](algorithm.assets/Dijkstra%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%20%ED%91%9C%ED%98%84.JPG)
+- 알고리즘 : Dijkstra
+    ![Dijkstra 알고리즘](algorithm.assets/dijkstra%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98.JPG)
