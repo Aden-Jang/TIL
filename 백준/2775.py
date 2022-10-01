96763a2403bb32409915d1e8303ef901f682927f
Aden-Jang <wrd4000@gmail.com>
2022-10-02 03:44:50 +0900

    221002 03:44 update

--- 백준/2775.py
+++ 백준/2775.py
@@ -1,5 +1,10 @@
 T = int(input())
+a = [[0] * 15 for _ in range(14)]
+a.append([i for i in range(15)])
+for i in range(13, -1, -1):
+    for j in range(1, 15):
+        a[i][j] = a[i+1][j] + a[i][j-1]
 for _ in range(T):
     k = int(input())
     n = int(input())
-    
+    print(a[14-k][n])
