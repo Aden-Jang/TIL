4737bed6243bf18e2815548c734ef36e21fa74f6
Aden-Jang <wrd4000@gmail.com>
2022-10-10 04:53:35 +0900

    221010 04:53 update

--- 백준/1009.py
+++ 백준/1009.py
@@ -1,11 +1,15 @@
-import sys
-input = sys.stdin.readline
+dic = {2:(2, 4, 8, 6), 3:(3, 9, 7, 1), 4:(4, 6), 7:(7, 9, 3, 1), 8:(8, 4, 2, 6), 9:(9, 1)}
+
 
 T = int(input())
 for _ in range(T):
     a, b = map(int, input().split())
-    ans = a
-    for i in range(b-1):
-        ans = (ans*a)%10
-
-    print(a)
+    a %= 10
+    if a in [1, 5, 6]:
+        print(a)
+    elif a in [2, 3, 7, 8]:
+        print(dic[a][(b % 4)-1])
+    elif a in [4, 9]:
+        print(dic[a][(b % 2)-1])
+    elif a == 0:
+        print(10)
